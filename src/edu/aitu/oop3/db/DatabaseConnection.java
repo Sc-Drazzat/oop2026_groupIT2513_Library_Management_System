aa2a1264815c29636814d0ede290b3e82d425772
Ayan Rakhimov <252013@astanait.edu.kz>
2026-01-15 08:31:21 +0500

    Ayan Rakhimov Set connection of table with Supabase

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseConnection {
     private static final String URL =
             "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
