93f25de150072a72a2a9b8c08e84cec62ee42b5e
Ayan Rakhimov <252013@astanait.edu.kz>
2026-01-18 15:05:18 +0500

    Ayan Rakhimov MemberNotFoundException created

---
+++ src/edu/aitu/oop3/exception/MemberNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.aitu.oop3.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+}
