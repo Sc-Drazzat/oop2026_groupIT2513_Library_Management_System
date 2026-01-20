15f9abba105b0e351aac3c790384bb9a4b9ad7a8
Ayan Rakhimov <252013@astanait.edu.kz>
2026-01-20 07:34:53 +0500

    Ayan Rakhimov BookRepository Interface updated

--- src/edu/aitu/oop3/repositories/BookRepository.java
+++ src/edu/aitu/oop3/repositories/BookRepository.java
@@ -3,7 +3,7 @@ import edu.aitu.oop3.entities.Book;
 import java.util.List;
 
 public interface BookRepository {
-    Book findById(int id);
+    Book findBookById(int id);
     List<Book> ListAvailableBooks();
     void addBook(Book book);
     void updateBook(Book book);
