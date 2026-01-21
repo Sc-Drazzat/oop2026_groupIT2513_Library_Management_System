ad6bb3bfdba081691112960521a6dfa825fec938
Ayan Rakhimov <252013@astanait.edu.kz>
2026-01-21 19:51:54 +0500

    Ayan Rakhimov BookRepository created

---
+++ src/edu/aitu/oop3/repositories/BookRepository.java
@@ -0,0 +1,88 @@
+package edu.aitu.oop3.repositories;
+import edu.aitu.oop3.entities.Book;
+import edu.aitu.oop3.db.DatabaseConnection;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class BookRepository {
+    public Book findBookById(int id) {
+        String query = "SELECT * FROM books WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return mapResultSetToBook(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    return null;
+    }
+
+    public List<Book> listAvailableBooks() {
+        List<Book> books = new ArrayList<>();
+        String query = "SELECT * FROM books WHERE available = TRUE";
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                books.add(mapResultSetToBook(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+
+    public void addBook(Book book) {
+        String query = "INSERT INTO books (title, author, available) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, book.getTitle());
+            stmt.setString(2, book.getAuthor());
+            stmt.setBoolean(3, book.isAvailable());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateBook(Book book) {
+        String query = "UPDATE books SET title = ?, author = ?, available = ? WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, book.getTitle());
+            stmt.setString(2, book.getAuthor());
+            stmt.setBoolean(3, book.isAvailable());
+            stmt.setInt(4, book.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteBook(int id) {
+        String query = "DELETE FROM books WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private Book mapResultSetToBook(ResultSet rs) throws SQLException {
+        return new Book(
+                rs.getInt("id"),
+                rs.getString("title"),
+                rs.getString("author"),
+                rs.getBoolean("available")
+        );
+    }
+}
+
+
+
